e823a7df88d691cfcf47b5dc3ebb929f34362cc4
yu.pan <panyupy@vip.qq.com>
2024-12-04 00:05:17 +0800

    feat: 页面初始化

---
+++ tracker-manage-web/src/routes/event-data-trend/chart.tsx
@@ -0,0 +1,24 @@
+import React, {useMemo} from "react";
+import {Line, LineConfig} from "@ant-design/plots";
+
+interface ChartProps {
+    data?: any[]
+}
+
+export const Chart: React.FC<ChartProps> = (props) => {
+    const {data} = props
+
+    const config: LineConfig = useMemo(() => {
+        return {
+            data: data ?? [],
+            xField: 'date',
+            yField: 'data',
+            seriesField: 'name',
+            legend: {position: 'top'},
+            smooth: true,
+            animation: {appear: {animation: 'path-in', duration: 2000}},
+        };
+    }, [data]);
+
+    return <Line {...config} />
+}
---
+++ tracker-manage-web/src/routes/event-data-trend/index.tsx
@@ -0,0 +1,40 @@
+import {Card, Col, Row, Statistic} from "antd";
+import {Chart} from "./chart";
+import React, {useEffect} from "react";
+import {service} from "../../service";
+import {useParams} from 'react-router';
+
+
+export const EventDataTrend = () => {
+
+    const {id} = useParams();  // 获取 URL 中的 id 参数
+
+    const [data, setData] = React.useState<any>({})
+
+    const fetchData = async () => {
+        const res = await service.fetchEventDataTrendData({id})
+        setData(res)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    return (
+        <div>
+            <Card title="今日数据">
+                <Row style={{textAlign: 'center'}}>
+                    <Col span={8}>
+                        <Statistic title="上报次数（PV）" value={data.pv}/>
+                    </Col>
+                    <Col span={8}>
+                        <Statistic title="上报次数（UV）" value={data.uv}/>
+                    </Col>
+                </Row>
+            </Card>
+            <Card title="上报统计">
+                <Chart data={data.trend}/>
+            </Card>
+        </div>
+    )
+}
