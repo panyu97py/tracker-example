75e080ac85e26aa635626ce1ce4ea64fbf890486
yu.pan <panyupy@vip.qq.com>
2024-12-05 01:29:33 +0800

    feat: 删除 layout 组件

--- tracker-manage-web/src/components/layout/context.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {RouteConfig} from "./types";
-
-export interface LayoutCtxVal {
-    registerRoute: (routeConfig: RouteConfig) => void;
-}
-
-export const LayoutCtx = React.createContext<Partial<LayoutCtxVal>>({})
--- tracker-manage-web/src/components/layout/types.ts
+++
@@ -1,6 +0,0 @@
-import {RouteProps} from "react-router-dom";
-
-export interface RouteExtData{
-    title: string;
-}
-export type RouteConfig = RouteExtData&RouteProps
