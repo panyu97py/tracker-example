f25a135d334534b709e1896c0e75ce9f375de310
yu.pan <panyupy@vip.qq.com>
2024-12-09 23:43:44 +0800

    feat: 增加缓存配置 5s

--- tracker-manage-server/src/app.module.ts
+++ tracker-manage-server/src/app.module.ts
@@ -4,10 +4,17 @@ import {AppService} from './app.service';
 import {EventConfigModule} from "@/modules/event-config/event-config.module";
 import {DatabaseModule} from "@/database/database.module";
 import {EventDataModule} from "@/modules/event-data/event-data.module";
+import {CacheModule} from '@nestjs/cache-manager';
 
 @Module({
-    imports: [EventConfigModule, EventDataModule, DatabaseModule],
+    imports: [
+        EventConfigModule,
+        EventDataModule,
+        DatabaseModule,
+        CacheModule.register({ttl: 5})
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
